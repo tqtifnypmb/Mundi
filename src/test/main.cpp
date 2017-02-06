8461dcde313845ddb6f167348346cc34f15917d8
tqtifnypmb <tqtifnypmb@gmail.com>
2017-02-06 19:05:52 +0800

    simple test

--- src/test/main.cpp
+++ src/test/main.cpp
@@ -14,6 +14,8 @@ void simple_test(const std::string& jstr) {
 int main()
 {
   simple_test({"true"});
+  simple_test({"TrUe"});
+  simple_test({"FaLSe"});
   simple_test({"123"});
   simple_test({ "[true, false, 1.234, 0.13e23, -1.2, -1]" });
   simple_test({ "[true, false, [1.23, 4, 6, 10], []]" });
